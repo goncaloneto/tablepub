a9637ce33b82c44f5d6b2dc2cca63081586b8960
Gonçalo Neto <gmneto@pentaho.com>
2017-02-01 17:21:54 +0000

    added password in db connection config file (cnx.php)

--- cnx.php
+++ cnx.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set("Europe/Lisbon");
-$conn = new mysqli("localhost", "goncaloneto", "", "pub"); //host, user, password, BD
 $conn->set_charset("utf8"); 
+$conn = new mysqli("localhost", "goncaloneto", "dianarute", "pub"); //host, user, password, BD
 
 if(mysqli_connect_errno()){ //errno - error number
   printf("Error:%s\n", mysqli_connect_error()); 
